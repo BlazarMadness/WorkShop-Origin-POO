c6d74a5ae71ddd232e5bcfcaed379102a56b0fdf
Keiner gonzalez <gkeiner22@gmail.com>
2024-09-29 11:15:04 -0500

    ejercicio 1
    cree la clase persona con los atributos "nombre" y "edad", con un método "saludar"

---
+++ PERSONA.ts
@@ -0,0 +1,12 @@
+class persona {
+    nombre: string;
+    edad: number;
+    constructor(nombre: string, edad: number){
+    this.nombre = nombre,
+    this.edad = edad;
+    }
+
+    saludar() {
+        console.log(`Tu nombre es ${this.nombre} y tienes ${this.edad} años.`);
+    }
+}
