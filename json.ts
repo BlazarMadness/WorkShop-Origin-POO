529b2dd05285a5539124be82dbf95923d9d734af
Keiner gonzalez <gkeiner22@gmail.com>
2024-10-01 15:13:18 -0500

    Ejercicio 11
    Cree un json con una lista de 10 empleados

---
+++ json.ts
@@ -0,0 +1,106 @@
+
+const empleadosjson = [
+    {
+        nombre: "Juan",
+        edad: 25,
+        direccion: {    
+            calle: "av. 1",
+            ciudad: "medellin",
+            pais: "Colombia"
+        },
+        vehiculo: []
+    },
+    {
+        nombre: "Pedro",
+        edad: 30,
+        direccion: {    
+            calle: "av. 2",
+            ciudad: "bogota",
+            pais: "Colombia"
+        },
+        vehiculo: []
+    },
+    {
+        nombre: "Maria",
+        edad: 35,
+        direccion: {    
+            calle: "av. 3",
+            ciudad: "medellin",
+            pais: "Colombia"
+        },
+        vehiculo: []
+    },
+    {
+        nombre: "Luis",
+        edad: 40,
+        direccion: {    
+            calle: "av. 4",
+            ciudad: "cali",
+            pais: "Colombia"
+        },
+        vehiculo: []
+    },
+    {
+        nombre: "Carlos",
+        edad: 45,
+        direccion: {    
+            calle: "av. 5",
+            ciudad: "barranquilla",
+            pais: "Colombia"
+        },
+        vehiculo: []
+    },
+    {
+        nombre: "Daniel",
+        edad: 50,
+        direccion: {    
+            calle: "av. 6",
+            ciudad: "santa marta",
+            pais: "Colombia"
+        },
+        vehiculo: []
+    },
+    {
+        nombre: "Ana",
+        edad: 55,
+        direccion: {    
+            calle: "av. 7",
+            ciudad: "villavicencio",
+            pais: "Colombia"
+        },
+        vehiculo: []
+    },
+    {
+        nombre: "Mariana",
+        edad: 60,
+        direccion: {    
+            calle: "av. 8",
+            ciudad: "bogota",
+            pais: "Colombia"
+        },
+        vehiculo: []
+    },
+    {
+        nombre: "Jorge",
+        edad: 65,
+        direccion: {    
+            calle: "av. 9",
+            ciudad: "pereira",
+            pais: "Colombia"
+        },
+        vehiculo: []
+    },
+    {
+        nombre: "Sofia",
+        edad: 70,
+        direccion: {    
+            calle: "av. 10",
+            ciudad: "cucuta",
+            pais: "Colombia"
+        },
+        vehiculo: []
+    }
+]
+
+const jsonEmpleados = JSON.stringify(empleadosjson);
+
